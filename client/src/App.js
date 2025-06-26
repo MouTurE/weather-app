20e44daa1d4d8bb017f4ad7c9a5c7df9826a1d5b
MouTurE <m.turkalp53@gmail.com>
2025-06-26 13:02:21 +0300

    Fetch Data and Add to Database

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,57 @@
 import './App.css';
 import axios from "axios";
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 
 function App() {
 
+  const inputRef = useRef();
+  const [debugLine, setDebugLine] = useState(""); 
   const [weatherDataList,setWeatherDataList] = useState([]);
+  const [weatherData, setWeatherData] = useState(false);
 
+  // const alIcons = {
+  //   "01d" :clear_icon,
+  //   "01n" : clear_icon,
+  //   "02d" : cloud_icon,
+  // }
+
+  const search = async (city) => {
+
+    if (city === "") {
+      setDebugLine("No city entered")
+      return;
+    }
+
+    try {
+
+      setDebugLine("Fetching data...");
+
+      const API_KEY = 'ea55d37a981e193fa1e393437482c8ae';
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      const processedData = {
+        humidity : data.main.humidity,
+        windSpeed : data.wind.speed,
+        temperature : Math.floor(data.main.temp),
+        location : data.name,
+
+      };
+
+      console.log(processedData);
+      setWeatherData(processedData);
+      axios.post("http://localhost:3001/weathers", processedData);
+      setWeatherDataList([...weatherDataList,processedData])
+
+      setDebugLine("Fetching Successful!");
+    }
+    catch (error) {
+     
+    }
+  }
+
+  // Updates the list according to the database
   useEffect(() => {
     axios.get("http://localhost:3001/weathers").then((response) => {
       setWeatherDataList(response.data)
@@ -13,38 +59,58 @@ function App() {
   }, []);
   
 
+
+
   return (
     <div className="App">
       <h1>Weather App</h1>
       
       <div className='getWeatherData'>
-        <input type='text' placeholder='Enter City...'/>
-        <button>Get City Data</button>
+        <input ref={inputRef} type='text' placeholder='Enter City...'/>
+        <button onClick={() => search(inputRef.current.value)}>Get City Data</button>
       </div>
-      <h2>Saved Data</h2>
-      {weatherDataList.map((weatherData, key) => {
-          return (
+      <p className='debugLine'> {debugLine} </p>
 
-            <table border={1} cellPadding={5} key={key}>
+      <div className='weatherDisplay'>
+           <p> Location: {weatherData.location} </p>
+           <p> Humidity: {weatherData.humidity} % </p>
+           <p>Wind Speed: {weatherData.windSpeed} Km/h </p>
+           <p> Temperature: {weatherData.temperature} °C</p>
+      </div>
+
+
+      <h2>Saved Data</h2>
+
+     
+
+            <table border={1} cellPadding={5}>
 
               <thead>
                 <tr>
-                  <th>City Name</th>
+                  <th>Location</th>
                   <th>Temperature</th>
-                  <th>Description</th>
+                  <th>Humidty</th>
+                  <th>Wind Speed</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>{weatherData.city}</td>
-                  <td>{weatherData.temperature}</td>
-                  <td>{weatherData.description}</td>
-                </tr>
+                {weatherDataList.map((weatherData, key) => {
+                  return(
+                    <tr key={key}>
+                      <td>{weatherData.location}</td>
+                      <td>{weatherData.temperature}</td>
+                      <td>{weatherData.humidity}</td>
+                      <td>{weatherData.windSpeed}</td>
+                    </tr>
+                  )
+                }
+                )}
               </tbody>
               
             </table>
-        );
-      })}
+          
+        
+      
     </div>
   );
 }
--- server/models/WeatherData.js
+++ server/models/WeatherData.js
@@ -1,8 +1,24 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('WeatherData', {
-    city: DataTypes.STRING,
-    temperature: DataTypes.FLOAT,
-    description: DataTypes.STRING,
-    date: DataTypes.DATEONLY
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    temperature: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    humidity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    windSpeed: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      defaultValue: DataTypes.NOW,
+    }
   });
 };
